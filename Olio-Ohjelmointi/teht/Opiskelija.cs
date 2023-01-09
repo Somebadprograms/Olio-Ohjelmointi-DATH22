671aabc6635284620e67d40b54d66b0eee54e76f
Arttu Nykänen <Arttu Nyknen@STUDY22P-771121>
2023-01-09 13:29:46 +0200

    2

---
+++ Olio-Ohjelmointi/teht/Opiskelija.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht
+{
+    internal class Opiskelija
+    {
+        private string Nimi;
+        private string Id;
+        private int Opintopisteet;
+
+        public Opiskelija(string nimi, string id, int opintopisteet)
+        {
+            Nimi = nimi;
+            Id = id;
+            Opintopisteet = opintopisteet;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Opintopisteet: " + Opintopisteet);
+
+        }
+
+        public void MuokkaaOpintopisteitä(int i)
+        {
+            Opintopisteet = i;
+        }
+    }
+}
---
+++ Olio-Ohjelmointi/teht/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static teht.Opiskelija;
+
+namespace teht
+{
+    internal class Program
+    {
+        internal class Progra
+        {
+            static void Main(string[] args)
+            {
+                List<Opiskelija> list = new List<Opiskelija>();
+                Opiskelija opiskelija0 = new Opiskelija("Elias", "Datanomi", 15);
+                opiskelija0.MuokkaaOpintopisteitä(20);
+
+                Opiskelija opiskelija1 = new Opiskelija("Ali", "Datanomi", 5);
+                Opiskelija opiskelija2 = new Opiskelija("Datanom", "Datanomi", 45);
+                Opiskelija opiskelija3 = new Opiskelija("Maito", "Datanomi", 25);
+
+                list.Add(opiskelija0);
+                list.Add(opiskelija1);
+                list.Add(opiskelija2);
+                list.Add(opiskelija3);
+
+                foreach (Opiskelija opiskelija in list)
+                {
+                    opiskelija.TulostaData();
+                    Console.WriteLine("");
+                }
+
+               
+                
+            }
+        }
+    }
+}
