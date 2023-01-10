f3668eecdc0aeb0e52420ba24e5bf5300e1d1c64
Arttu Nykänen <Arttu Nyknen@STUDY22P-771121>
2023-01-10 11:32:36 +0200

    7

---
+++ Olio-Ohjelmointi/Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+            while (true)
+            {
+                Console.Write("Which floor would you like to go to (1-6)? ");
+                int targetFloor = int.Parse(Console.ReadLine());
+                elevator.CurrentFloor = targetFloor;
+            }
+        }
+    }
+}
---
+++ Olio-Ohjelmointi/Elevator/elevator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    internal class Elevator
+    {
+        private int _currentFloor;
+        public int CurrentFloor
+        {
+            get
+            {
+                return _currentFloor;
+            }
+            set
+            {
+                if (value >= 1 && value <= 6)
+                {
+                    _currentFloor = value;
+                    Console.WriteLine("Elevator moving to floor " + _currentFloor);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid floor. Please enter a number between 1 and 6.");
+                }
+            }
+        }
+        public Elevator()
+        {
+            _currentFloor = 1;
+        }
+    }
+}
+
